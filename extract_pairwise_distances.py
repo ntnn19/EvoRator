8ae2f1d41c84c1de411a48ffb3f7f12669f38d81
ntnn19 <natannagar89@gmail.com>
2025-04-08 10:02:03 +0200

    cleaned the code a bit

--- extract_pairwise_distances.py
+++ extract_pairwise_distances.py
@@ -6,15 +6,6 @@ import obtain_pdb
 import matplotlib.pyplot as plt
 import click
 
-laptop = False if os.path.exists("/groups/pupko/natannag") else True
-
-if laptop:
-    path2github = "../"
-else:
-    path2github = "/groups/pupko/natannag"
-
-sys.path.append(path2github)
-
 def get_edge_list(identifier,backbone_coordinates,residue_pdb_index,job_dir):
     output_name_all = identifier.split("_")[0].upper()+"_edgelist.txt"
     output_all = os.path.join(job_dir,output_name_all)
@@ -27,19 +18,15 @@ def get_edge_list(identifier,backbone_coordinates,residue_pdb_index,job_dir):
                 Calpha_Calpha_distance <= float(upper_cutoff))
     Calpha_Calpha_distance_filtered = Calpha_Calpha_distance_filtered.astype(int)
     print(Calpha_Calpha_distance_filtered.shape)
-    plt.matshow(Calpha_Calpha_distance_filtered)
-    plt.show()
     plt.savefig(os.path.join(job_dir,'debug_naps.png'))
     residue_pdb_index_with_chain_id = []
 
     for i in residue_pdb_index:
-        print(i)
         residue_pdb_index_with_chain_id.append(str(i[1])+str(i[-1]))
     int2label = dict(zip(list(range(Calpha_Calpha_distance_filtered.shape[0])), residue_pdb_index_with_chain_id))
     G = nx.from_numpy_matrix(Calpha_Calpha_distance_filtered)
     G = nx.relabel_nodes(G, int2label)
     G.remove_edges_from(nx.selfloop_edges(G))
-    print(G.nodes)
 
 
     nx.write_edgelist(G,output_all , data=False,delimiter='\t')
